afd49c7ff00a7f0ef8fa0d7e15db4feaf611578f
Nicola <nicregattieri@gmail.com>
2019-07-26 17:17:34 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../App.css";
+// import spinner from "../ajax-loader.gif";
+// import SearchResult from "./SearchResult";
+import Search from "./Search";
+import SearchResult from "./SearchResult";
+
+const App = () => {
+  const [[products], setProducts] = useState("");
+
+  const search = searchValue => {
+    fetch(
+      `http://www.cljoias.vtexcommercestable.com.br/api/catalog_system/pub/products/search/${searchValue}`,
+      {
+        method: "GET",
+        headers: {
+          cookie: "janus_sid=aac43e0c-ab16-4455-80ae-5dfd9e22c48b"
+        }
+      }
+    )
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(err => console.log(err));
+
+    console.log(products);
+  };
+
+  return (
+    <div className="App">
+      <Search search={search} />
+      <SearchResult />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SearchResult.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const searchResult = ({ product }) => {
+  return (
+    <div className="product">
+      {/* <span className="product__id" data-product-id={product.productId}>
+        {" "}
+      </span>
+      <div className="product__media">
+        <a className="product__link" href="${link}">
+          <img src={product.thumb} alt={product.productName} />
+        </a>
+      </div>
+      <div className="product__info">
+        <h3 className="product__name">
+          <a
+            className="product__link"
+            title={product.productName}
+            href={product.link}
+          >
+            {product.productName}
+          </a>
+        </h3>
+      </div>
+      <div className="product__content">
+        <div className="product__price">
+          <div className="price">
+            <span className="price__best">{product.bestPriceFormated}</span>
+            <span className="price__unit"> {product.multiplier}</span>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default searchResult;
